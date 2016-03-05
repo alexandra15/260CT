381c9fe929b39598f70f89a4eab8c67011c149e4
For A Lack Of A Better Name <dradazer@gmail.com>
2016-03-05 20:18:42 +0000

    Update Book A Trip.cs
    Skeleton for BookTrip class added

--- Book A Trip.cs
+++ Book A Trip.cs
@@ -1,1 +1,36 @@
+class BookTrip
+{
+  private int refNo;
+  private string journeyDestination;
+  private int journeyCost;
+  //need to find a date and time type for these
+  private int journeyTime;
+  private string journeyDate;
 
+  public string getJourneyDestination()
+  {
+    return journeyDestination;
+  }
+  public int getJourneyTime()
+  {
+    return journeyTime;
+  }
+  public int getJourneyCost()
+  {
+    return journeyCost;
+  }
+  public int getRefNo()
+  {
+    return refNo;
+  }
+  public void bookTrip()
+  {
+    //code goes here
+    refNo += 1;
+    getRefNo();
+  }
+  public void enterPaymentDetails()
+  {
+    //code goes here
+  }
+}
